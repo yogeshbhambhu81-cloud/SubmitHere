3910e4bd33cd6e192793e067aa8525e1dd6cdd06
Yogesh Bhambhu <yogeshbhambhu81@gmail.com>
2026-03-16 23:53:21 +0530

    mongo URl change

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
+console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+console.log("MONGO_URI preview:", process.env.MONGO_URI?.split("@")[1]);
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected to universityData"))
   .catch((err) => console.log("❌ DB Connection Error:", err));
 
